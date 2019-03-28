585fff16c84ac0ae8472502a8dacfd316811d100
seike460 <seike460@gmail.com>
2019-03-28 16:25:56 +0900

    :+1: cli

--- cli.go
+++ cli.go
@@ -4,25 +4,24 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/fictionbase/fictionbase/type/fbprocess"
 	"github.com/fictionbase/fictionbase/type/fbresource"
 )
 
-type fictionBase interface {
-	Run()
+type MessageBase struct {
+	TypeKey    string    `json:"type_key"`
+	StorageKey string    `json:"storage_key"`
+	TimeKey    time.Time `json:"time_key"`
 }
 
 // RunCLI exec Cli
 func RunCLI(i []interface{}) int {
-	// @TODO fictionBase return by config want Run
-	var fictionBase []fbresource.FictionBase
 	resource := fbresource.FictionBase{}
-	fictionBase = append(fictionBase, resource)
-	// loop goroutine on background
-	for _, fiction := range fictionBase {
-		go fiction.Run()
-	}
-
+	resource.Run()
+	processes := fbprocess.FictionBase{}
+	processes.Run()
 	signalCh := make(chan os.Signal)
 	signal.Notify(signalCh, syscall.SIGHUP, syscall.SIGTERM, os.Interrupt)
 	select {
